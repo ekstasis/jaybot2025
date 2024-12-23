9198b2190db7c8b2011328e57c53d56aa16b73d4
James Baxter <8950586+ekstasis@users.noreply.github.com>
2024-12-23 15:37:12 -0500

    Initial commit

---
+++ Ryan Bowls Files/mongodb.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+import urllib.parse
+
+username = urllib.parse.quote_plus("AdminBaxter")
+password = urllib.parse.quote_plus("jaybot123")
+
+
+def connection():
+    client = MongoClient('mongodb://%s:%s@137.184.72.86:27052/' % (username, password))
+    collection = client.jaybot
+    return collection
