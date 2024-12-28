b83ed72e70858af6d97f63556cb91902a679ce82
James Baxter <8950586+ekstasis@users.noreply.github.com>
2024-12-27 22:27:40 -0500

    Can specify single product

--- price_change_monitor.py
+++ price_change_monitor.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime, timedelta, timezone
+
 from apscheduler.schedulers.background import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 from pymongo.synchronous.collection import Collection
@@ -8,7 +9,7 @@ import config
 from mongodb import get_database
 
 
-def print_price_change(collection: Collection, interval: timedelta):
+def print_price_change(collection: Collection, interval: timedelta, product: str = None):
     """Prints the % change in price over the last minute for each product
     """
     # Calculate edges of previous minute
@@ -16,9 +17,14 @@ def print_price_change(collection: Collection, interval: timedelta):
     end_time = now.replace(second=0, microsecond=0)
     start_time = end_time - interval
 
+    match = {"time": {"$gte": start_time, "$lt": end_time}}
+
+    if product is not None:
+        match["product_id"] = product
+
     # filter, sort, group, calculate
     pipeline = [
-        {"$match": {"time": {"$gte": start_time, "$lt": end_time}}},
+        {"$match": match},
         {"$sort": {"time": 1}},
         {"$group": {  # returns one document for each product
             "_id": "$product_id",
@@ -41,36 +47,45 @@ def print_price_change(collection: Collection, interval: timedelta):
 
     start_str = start_time.strftime('%H:%M')
     end_str = end_time.strftime('%H:%M')
-    print(f'\n{start_str} -> {end_str}\n==============')
+    print(f'{start_str} -> {end_str}\n==============')
     price_changes_per_product = list(collection.aggregate(pipeline))
+
     if not price_changes_per_product:
-        print("No Trades")
+        print("NO TRADES\n")
         return
 
     for product in price_changes_per_product:
         dec_chg = product['chg_pct'].to_decimal()
         print(f"{product['_id']}:  {float(dec_chg):.2f}%")
 
+    print()
+    return
 
-def minutely_report(collection: Collection):
-    print_price_change(collection=collection, interval=timedelta(minutes=1))
+
+def minutely_report(collection: Collection, product: str = None):
+    print_price_change(collection=collection, interval=timedelta(minutes=1), product=product)
 
 
-def hourly_report(collection: Collection):
-    print_price_change(collection=collection, interval=timedelta(hours=1))
+def hourly_report(collection: Collection, product: str = None):
+    print_price_change(collection=collection, interval=timedelta(hours=1), product=product)
+
 
 if __name__ == '__main__':
 
     logging.basicConfig()
-    logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+    logging.getLogger('apscheduler').setLevel(logging.WARNING)
 
     db = get_database('jaybot')
     trades = db.get_collection(config.collection_name)
 
     print(f'\nUsing collection: \"{config.collection_name}\"\n')
 
+    print("Test run before we begin:\n")
+    minutely_report(trades, product='XLM-USD')
+
     scheduler = BlockingScheduler()
     scheduler.add_job(minutely_report, CronTrigger(minute="0-59"), args=[trades])
     scheduler.add_job(hourly_report, CronTrigger(minute="0"), args=[trades])
     scheduler.print_jobs()
+    print()
     scheduler.start()
