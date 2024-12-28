0716f710ba5dcfcb359344f9c87c8e067164278a
James Baxter <8950586+ekstasis@users.noreply.github.com>
2024-12-27 21:22:42 -0500

    Use APScheduler, add hourly reports

--- price_change_monitor.py
+++ price_change_monitor.py
@@ -1,12 +1,14 @@
-import time
+import logging
 from datetime import datetime, timedelta, timezone
-import schedule
+from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from pymongo.synchronous.collection import Collection
 
+import config
 from mongodb import get_database
-from config import collection_name
 
 
-def print_price_change(collection: dict, interval: timedelta):
+def print_price_change(collection: Collection, interval: timedelta):
     """Prints the % change in price over the last minute for each product
     """
     # Calculate edges of previous minute
@@ -39,7 +41,7 @@ def print_price_change(collection: dict, interval: timedelta):
 
     start_str = start_time.strftime('%H:%M')
     end_str = end_time.strftime('%H:%M')
-    print(f'\n=== {start_str} -> {end_str} ===')
+    print(f'\n{start_str} -> {end_str}\n==============')
     price_changes_per_product = list(collection.aggregate(pipeline))
     if not price_changes_per_product:
         print("No Trades")
@@ -47,30 +49,28 @@ def print_price_change(collection: dict, interval: timedelta):
 
     for product in price_changes_per_product:
         dec_chg = product['chg_pct'].to_decimal()
-        print(f"{product['_id']} 1-minute price change:  {float(dec_chg):.2f}%")
+        print(f"{product['_id']}:  {float(dec_chg):.2f}%")
 
+
+def minutely_report(collection: Collection):
+    print_price_change(collection=collection, interval=timedelta(minutes=1))
+
+
+def hourly_report(collection: Collection):
+    print_price_change(collection=collection, interval=timedelta(hours=1))
 
 if __name__ == '__main__':
-    """
-    Every 60 seconds, report previous minute's price change for each product
-    """
-    trades = get_database('jaybot')[collection_name]
 
-    one_minute = timedelta(minutes=1)
-    one_hour = timedelta(hours=1)
+    logging.basicConfig()
+    logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
-    # schedule.every().minute.do(print_price_change, collection=trades, interval=one_minute)
-    schedule.every().hour.do(print_price_change, collection=trades, interval=one_hour)
+    db = get_database('jaybot')
+    trades = db.get_collection(config.collection_name)
 
-    print(f'\n\nUsing collection: \"{collection_name}\"')
-    seconds_till = schedule.idle_seconds()
-    if seconds_till < 60: # ?  f'{seconds_till:0f}' : f'{seconds_till / 60:1f}'
-        print(f"Next job in {seconds_till:.0f} seconds")
-    else:
-        print(f"Next job in {seconds_till / 60:.1f} minutes")
+    print(f'\nUsing collection: \"{config.collection_name}\"\n')
 
-    print(schedule.next_run())
-
-    while True:
-        schedule.run_pending()
-        time.sleep(60)
+    scheduler = BlockingScheduler()
+    scheduler.add_job(minutely_report, CronTrigger(minute="0-59"), args=[trades])
+    scheduler.add_job(hourly_report, CronTrigger(minute="0"), args=[trades])
+    scheduler.print_jobs()
+    scheduler.start()
